5e51efd0a8222eb0f49b3c33d96e5f901033cdb1
Maryary94 <veoliveira94@gmail.com>
2020-05-09 16:23:05 +0100

    Add

--- Trabalho_Udemy_Vera/Indecition_app/src/app.js
+++
@@ -1,8 +0,0 @@
-console.log("App.js is running");
-
-//JSX - JavaScript XML
-var template = <p>This is JSX app.js</p>; //React.createElement("h1", { id: "someid" }, "Something new");
-var appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
-
-//babel src/app.js --out-file=public/scripts/app.js --presets=env,react
