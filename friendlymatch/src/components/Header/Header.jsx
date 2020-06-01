ce93a2b2bd23a2bdc9a56d1da44b98a3b0ddad90
Maryary94 <veoliveira94@gmail.com>
2020-06-01 19:59:04 +0100

    Views

--- friendlymatch/src/components/Header/Header.jsx
+++ friendlymatch/src/components/Header/Header.jsx
@@ -8,16 +8,7 @@ export default function Header(props) {
       <div className="header">
         <i className={`${props.iconLeft} iconFontAwesome `}></i>
         <span className="text">{props.title}</span>
-        {/*
-        <div className="x">{props.x}</div>
-        <div className="back">{props.back}</div>
-        <div className="backAndSettings">{props.backAndSettings}</div>
-        <div className="menuAndSettings">{props.menuAndSettings}</div>
-        <div className="xAndCheck">{props.xAndCheck}</div>
-        <div className="xAndSend">{props.xAndSend}</div>
-        */}
         <i className={`${props.iconRight} iconFontAwesome `}></i>
-        {/*disabled={props.disabled}*/}
       </div>
     </>
   );
