b2bf47158d2c62ba2c9f2860e7a9e5e8c9cb9cf6
Maryary94 <veoliveira94@gmail.com>
2020-06-18 18:09:36 +0100

    Update

--- friendlymatch/src/views/Game/AddPlayers.jsx
+++ friendlymatch/src/views/Game/AddPlayers.jsx
@@ -1,5 +1,46 @@
 import React from "react";
+import Header from "../../components/Header/Header";
+import Button from "../../components/Form/Button/Button";
+import "./AddPlayers.css";
+import {
+  PersonAdd,
+  Facebook,
+  Instagram,
+  Twitter,
+  Link,
+} from "@material-ui/icons";
 
+import { Divider } from "@material-ui/core";
 export default function AddPlayers() {
-  return <div></div>;
+  return (
+    <>
+      <Header
+        // iconLeft={"fas fa-times"}
+        title={"Create New Game"}
+        // iconRight={"fas fa-check"}
+      ></Header>
+      <div className="AddPlayers">
+        <div>
+          <p>
+            <b>
+              <PersonAdd></PersonAdd> Add Players
+            </b>
+          </p>
+
+          <b>Send a request to join</b>
+        </div>
+      </div>
+
+      <Divider />
+      <div className="WaysToSendInvitations">
+        <Facebook></Facebook>
+        <Instagram></Instagram>
+        <Twitter></Twitter>
+        <Link></Link>
+      </div>
+      <div className="ContinueButton">
+        <Button name="Continue" type="submit" btnColor="success" />
+      </div>
+    </>
+  );
 }
--- friendlymatch/src/views/Group/AddMembers.jsx
+++ friendlymatch/src/views/Group/AddMembers.jsx
@@ -1,5 +1,46 @@
 import React from "react";
+import Header from "../../components/Header/Header";
+import Button from "../../components/Form/Button/Button";
+import "./AddMembers.css";
+import {
+  PersonAdd,
+  Facebook,
+  Instagram,
+  Twitter,
+  Link,
+} from "@material-ui/icons";
 
+import { Divider } from "@material-ui/core";
 export default function AddMembers() {
-  return <div></div>;
+  return (
+    <>
+      <Header
+        iconLeft={"fas fa-times"}
+        title={"Create New Group"}
+        iconRight={"fas fa-check"}
+      ></Header>
+      <div className="AddPlayersGroup">
+        <div>
+          <p>
+            <b>
+              <PersonAdd></PersonAdd> Add Players
+            </b>
+          </p>
+
+          <b>Send a request to join</b>
+        </div>
+      </div>
+
+      <Divider />
+      <div className="WaysToSendInvitationsGroup">
+        <Facebook></Facebook>
+        <Instagram></Instagram>
+        <Twitter></Twitter>
+        <Link></Link>
+      </div>
+      <div className="ContinueButtonGroup">
+        <Button name="Continue" type="submit" btnColor="success" />
+      </div>
+    </>
+  );
 }
