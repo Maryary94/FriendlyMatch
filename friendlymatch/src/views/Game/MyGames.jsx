06a73522228b0af460d73fd214bd0e7faeea93fa
Maryary94 <veoliveira94@gmail.com>
2020-06-18 10:23:04 +0100

    Update

--- friendlymatch/src/views/Game/MyGames.jsx
+++ friendlymatch/src/views/Game/MyGames.jsx
@@ -1,17 +1,56 @@
-import React  from "react";
-
+import React from "react";
+import Header from "../../components/Header/Header";
+import Menu from "../../components/Menu/Menu";
+import NavBar from "../../components/Form/ButtonGroup/Dashboard/menuDashboard";
+import ProfilePicture from "../../components/ProfilePicture/ProfilePicture";
+import AddIcon from "@material-ui/icons/Add";
+import { Button, Paper, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import "./MyGames.css";
-import { withFirebase } from "../../services";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
-function MyGames({firebase}) {
-    
-    return (
-        <>
-        <div>
-            MyGames
+export default function MyGames() {
+  const classes = useStyles();
+  return (
+    <>
+      <Header>
+        <Menu />
+      </Header>
+      <ProfilePicture />
+      <NavBar />
+
+      <div className={classes.root}>
+        <div className="GridTitle">
+          <Grid container spacing={3}>
+            <Grid item xs={6} sm={3}>
+              <Paper className={classes.paper}>
+                <b>My Games </b>
+              </Paper>
+            </Grid>
+          </Grid>
+          {/* Fazer uma lista das groups que estão na base de dados*/}
+          <div className="listGroups">
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.paper}>
+                <b>Name of the game </b>
+                <p> Name of the Group: </p>
+                <Button variant="contained" className="CreateGroup">
+                  visit
+                </Button>
+              </Paper>
+            </Grid>
+          </div>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
-
-export default withFirebase(MyGames);
--- friendlymatch/src/views/Group/MyGroups.jsx
+++ friendlymatch/src/views/Group/MyGroups.jsx
@@ -3,16 +3,58 @@ import Header from "../../components/Header/Header";
 import Menu from "../../components/Menu/Menu";
 import NavBar from "../../components/Form/ButtonGroup/Dashboard/menuDashboard";
 import ProfilePicture from "../../components/ProfilePicture/ProfilePicture";
+import AddIcon from "@material-ui/icons/Add";
+import { Button, Paper, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import "./MyGroups.css";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 export default function MyGroups() {
+  const classes = useStyles();
   return (
     <>
-      <div className="rowMenu">
-        <Header>
-          <Menu />
-        </Header>
-        <ProfilePicture />
-        <NavBar />
+      <Header>
+        <Menu />
+      </Header>
+      <ProfilePicture />
+      <NavBar />
+
+      <div className={classes.root}>
+        <div className="GridTitle">
+          <Grid container spacing={3}>
+            <Grid item xs={6} sm={3}>
+              <Paper className={classes.paper}>
+                <b>My Groups </b>
+              </Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <Button variant="contained" className="CreateGroup">
+                Create group <AddIcon></AddIcon>
+              </Button>
+            </Grid>
+          </Grid>
+          {/* Fazer uma lista das groups que estão na base de dados*/}
+          <div className="listGroups">
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.paper}>
+                <b>Name of the group </b>
+                <p> Admin Name: </p>
+                <Button variant="contained" className="CreateGroup">
+                  visit
+                </Button>
+              </Paper>
+            </Grid>
+          </div>
+        </div>
       </div>
     </>
   );
