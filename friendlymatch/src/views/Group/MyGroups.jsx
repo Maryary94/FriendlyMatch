299f6a9139c1bdeac94eb37657d1c52ba7d50b2b
Diogo <diogo_sasantos@hotmail.com>
2020-06-19 01:30:16 +0100

    Listar Grupos

--- friendlymatch/src/views/Group/MyGroups.jsx
+++ friendlymatch/src/views/Group/MyGroups.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Header from "../../components/Header/Header";
 import Menu from "../../components/Menu/Menu";
 import NavBar from "../../components/Form/ButtonGroup/Dashboard/menuDashboard";
@@ -7,6 +7,8 @@ import AddIcon from "@material-ui/icons/Add";
 import { Button, Paper, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import "./MyGroups.css";
+import { withFirebase } from "../../services";
+import { useEffect } from "react";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -18,8 +20,31 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function MyGroups() {
+
+function MyGroups({firebase}) {
   const classes = useStyles();
+  const currentUser = useRef();
+  const players = useRef();
+  const [playerGroups, setPlayerGroups] = useState({});
+
+  useEffect(()=>{
+    firebase.database().ref("Users").once("value").then((snapshot)=>{
+      players.current = snapshot.val();
+      firebase.database().ref("Groups").on("value", (snapshot)=>{
+        currentUser.current = firebase.auth().currentUser;
+        console.log(currentUser.current);
+        let allGroups = snapshot.val();
+        setPlayerGroups(Object.keys(allGroups)
+          .filter(group => allGroups[group].members.includes(currentUser.current.uid))
+          .reduce((obj, key) => {
+            obj[key] = allGroups[key];
+            return obj;
+          }, {}));
+      });
+    })
+  }, [firebase]);
+
+
   return (
     <>
       <Header>
@@ -43,19 +68,26 @@ export default function MyGroups() {
             </Grid>
           </Grid>
           {/* Fazer uma lista das groups que estão na base de dados*/}
-          <div className="listGroups">
-            <Grid item xs={12} sm={6}>
-              <Paper className={classes.paper}>
-                <b>Name of the group </b>
-                <p> Admin Name: </p>
-                <Button variant="contained" className="CreateGroup">
-                  visit
-                </Button>
-              </Paper>
-            </Grid>
-          </div>
+          {Object.keys(playerGroups).map(
+            (groupKey) => (
+              <div className="listGroups" key={groupKey}>
+                <Grid item xs={12} sm={6}>
+                  <Paper className={classes.paper}>
+                    <b> {playerGroups[groupKey].name} </b>
+                    <p> {players.current[playerGroups[groupKey].administrators[0]].firstName} </p>
+                    <Button variant="contained" className="CreateGroup">
+                      visit
+                    </Button>
+                  </Paper>
+                </Grid>
+              </div>
+            )
+          )}
+          
         </div>
       </div>
     </>
   );
 }
+
+export default withFirebase(MyGroups);
