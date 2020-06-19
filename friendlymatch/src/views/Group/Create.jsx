0cea8fb95aaa53209093c595561942305dfc01b4
Diogo Santos <diogo.santos@fabamaq.com>
2020-06-19 14:01:29 +0100

    Criar gupo

--- friendlymatch/src/views/Group/Create.jsx
+++ friendlymatch/src/views/Group/Create.jsx
@@ -4,8 +4,28 @@ import Header from "../../components/Header/Header";
 import Input from "../../components/Form/Input/Input";
 import UpdateImage from "../../components/UploadImage/UploadImage";
 import Button from "../../components/Form/Button/Button";
+import { withFirebase } from "../../services";
+import { useRef } from "react";
 
-export default function Create() {
+function Create({firebase, history}) {
+  const name = useRef();
+
+  const handleCreateGroup = () => {
+    var currentUser = firebase.auth().currentUser;
+    console.log("handleCreateGroup", currentUser);
+    firebase.database().ref("Groups").push({ 
+      name: name.current,
+      members: [currentUser.uid],
+      administrators: [currentUser.uid]
+    }, (error) => {
+      if(error){
+        console.error(error);
+      } else {
+        history.push("/MyGroups");
+      }
+    });
+  }
+
   return (
     <>
       <Header
@@ -16,15 +36,17 @@ export default function Create() {
         <p>
           <b>Create New Group</b>
         </p>
-        <Input id="GroupName" type="text" placeholder="Name of the Group" />
+        <Input id="GroupName" type="text" placeholder="Name of the Group" onChange={e => name.current = e.target.value}/>
         <div className="rowCreateGroup">
           <small>Adicionar imagem ao grupo </small>
           <UpdateImage></UpdateImage>
         </div>
       </div>
       <div className="ButtonCreateGroup">
-        <Button id="btn-verify" name="Confirm" btnColor="success" />
+        <Button id="btn-verify" name="Confirm" btnColor="success" onClick={handleCreateGroup}/>
       </div>
     </>
   );
 }
+
+export default withFirebase(Create)
