a3711fca471ef3b2754e9a05da007a7067104e2c
Maryary94 <veoliveira94@gmail.com>
2020-06-21 20:15:57 +0100

    Updates

--- friendlymatch/src/views/Game/AddPlayers.jsx
+++ friendlymatch/src/views/Game/AddPlayers.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import Close from "../../components/Icons/Close/Close";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import {
+  Paper,
+  Grid,
+  Divider,
+  Input,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  Chip,
+} from "@material-ui/core";
+
 import Button from "../../components/Form/Button/Button";
 import "./AddPlayers.css";
 import {
@@ -9,37 +23,140 @@ import {
   Twitter,
   Link,
 } from "@material-ui/icons";
-import { Divider } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    maxWidth: 300,
+  },
+  chips: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  chip: {
+    margin: 2,
+  },
+  noLabel: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  "Oliver Hansen",
+  "Van Henry",
+  "April Tucker",
+  "Ralph Hubbard",
+  "Omar Alexander",
+  "Carlos Abbott",
+  "Miriam Wagner",
+  "Bradley Wilkerson",
+  "Virginia Andrews",
+  "Kelly Snyder",
+];
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
 
 export default function AddPlayers() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState([]);
+
+  const handleChange = (event) => {
+    setPersonName(event.target.value);
+  };
   return (
     <>
-      <Header
-        // iconLeft={"fas fa-times"}
-        title={"Create New Game"}
-        // iconRight={"fas fa-check"}
-      ></Header>
-      <div className="AddPlayers">
-        <div>
-          <p>
-            <b>
-              <PersonAdd></PersonAdd> Add Players
-            </b>
-          </p>
+      <Header>
+        <Close link="/GroupMembers" />
+      </Header>
 
-          <b>Send a request to join</b>
+      <div className={classes.root}>
+        <div className="GridTitleGame">
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.paper}>
+                <b>
+                  <PersonAdd></PersonAdd> Add Players
+                </b>
+                <p>
+                  <b>Send a request to join</b>
+                </p>
+              </Paper>
+            </Grid>
+          </Grid>
         </div>
       </div>
 
       <Divider />
-      <div className="WaysToSendInvitations">
+      <div className="WaysToSendInvitationsGroup">
         <Facebook></Facebook>
         <Instagram></Instagram>
         <Twitter></Twitter>
         <Link></Link>
       </div>
-      <div className="ContinueButton">
-        <Button name="Continue" type="submit" btnColor="success" />
+      <Divider />
+      <Divider />
+      <Divider />
+      <div className="MembersNames">
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-mutiple-chip-label">Players Names</InputLabel>
+          <Select
+            labelId="mutiple-members"
+            id="mutiple-members"
+            multiple
+            value={personName}
+            onChange={handleChange}
+            input={<Input id="select-multiple-chip" />}
+            renderValue={(selected) => (
+              <div className={classes.chips}>
+                {selected.map((value) => (
+                  <Chip key={value} label={value} className={classes.chip} />
+                ))}
+              </div>
+            )}
+            MenuProps={MenuProps}
+          >
+            {names.map((name) => (
+              <MenuItem
+                key={name}
+                value={name}
+                style={getStyles(name, personName, theme)}
+              >
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <div className="ContinueButtonGroup">
+        <Button name="Add Players" type="submit" btnColor="success" />
       </div>
     </>
   );
--- friendlymatch/src/views/Group/AddMembers.jsx
+++ friendlymatch/src/views/Group/AddMembers.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Close from "../../components/Icons/Close/Close";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import {
+  Paper,
+  Grid,
+  Divider,
+  Input,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  Chip,
+} from "@material-ui/core";
 
 import Button from "../../components/Form/Button/Button";
 import "./AddMembers.css";
@@ -12,22 +24,93 @@ import {
   Link,
 } from "@material-ui/icons";
 
-import { Divider } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    maxWidth: 300,
+  },
+  chips: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  chip: {
+    margin: 2,
+  },
+  noLabel: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  "Oliver Hansen",
+  "Van Henry",
+  "April Tucker",
+  "Ralph Hubbard",
+  "Omar Alexander",
+  "Carlos Abbott",
+  "Miriam Wagner",
+  "Bradley Wilkerson",
+  "Virginia Andrews",
+  "Kelly Snyder",
+];
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
 export default function AddMembers() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState([]);
+
+  const handleChange = (event) => {
+    setPersonName(event.target.value);
+  };
   return (
     <>
-      <Header title={"Create New Group"}>
+      <Header>
         <Close link="/GroupMembers" />
       </Header>
-      <div className="AddPlayersGroup">
-        <div>
-          <p>
-            <b>
-              <PersonAdd></PersonAdd> Add Players
-            </b>
-          </p>
 
-          <b>Send a request to join</b>
+      <div className={classes.root}>
+        <div className="GridTitleGame">
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.paper}>
+                <b>
+                  <PersonAdd></PersonAdd> Add Members
+                </b>
+                <p>
+                  <b>Send a request to join</b>
+                </p>
+              </Paper>
+            </Grid>
+          </Grid>
         </div>
       </div>
 
@@ -38,8 +121,42 @@ export default function AddMembers() {
         <Twitter></Twitter>
         <Link></Link>
       </div>
+      <Divider />
+      <Divider />
+      <Divider />
+      <div className="MembersNames">
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-mutiple-chip-label">Members Names</InputLabel>
+          <Select
+            labelId="mutiple-members"
+            id="mutiple-members"
+            multiple
+            value={personName}
+            onChange={handleChange}
+            input={<Input id="select-multiple-chip" />}
+            renderValue={(selected) => (
+              <div className={classes.chips}>
+                {selected.map((value) => (
+                  <Chip key={value} label={value} className={classes.chip} />
+                ))}
+              </div>
+            )}
+            MenuProps={MenuProps}
+          >
+            {names.map((name) => (
+              <MenuItem
+                key={name}
+                value={name}
+                style={getStyles(name, personName, theme)}
+              >
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
       <div className="ContinueButtonGroup">
-        <Button name="Continue" type="submit" btnColor="success" />
+        <Button name="Add Members" type="submit" btnColor="success" />
       </div>
     </>
   );
