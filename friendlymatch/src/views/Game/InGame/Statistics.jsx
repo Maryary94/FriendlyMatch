23013181c38f76697a23301bfcb7e81978e7ff26
Diogo <diogo_sasantos@hotmail.com>
2020-06-22 00:12:06 +0100

    Teams

--- friendlymatch/src/views/Game/InGame/Statistics.jsx
+++ friendlymatch/src/views/Game/InGame/Statistics.jsx
@@ -21,17 +21,57 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.text.secondary,
   },
 }));
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 function Teams({firebase}) {
   const classes = useStyles();
   const { gameId } = useParams();
+  const [users, setUsers] = useState({});
   const [game, setGame] = useState({});
+  const [teams, setTeams] = useState([]);
 
   useEffect(() => {
     firebase
       .database()
       .ref("Games")
       .child(gameId)
-      .on("value", (snapshot) => setGame(snapshot.val()));
+      .on("value", (snapshot) => {
+        const gameSnapshot = snapshot.val();
+        const players = gameSnapshot.players||[];
+        const _mixedPlayers = shuffle(players);
+        const half = Math.ceil(players.length/2);
+        const teamsArr = [
+          _mixedPlayers.slice(0, half),
+          _mixedPlayers.slice(half)
+        ];
+        
+        firebase
+          .database()
+          .ref("Users")
+          .on("value", (snapshot) => {
+            setUsers(snapshot.val());
+            setTeams(teamsArr)
+            setGame(gameSnapshot);
+          });
+      });
   }, [firebase, gameId]);
   
   return (
@@ -70,46 +110,34 @@ function Teams({firebase}) {
               </Paper>
             </Grid>
           </Grid>
-          <Grid item xs={6} sm={3}>
-            <Paper className={classes.paper}>
-              <b>Team 1</b>
-            </Paper>
-          </Grid>
+          {teams.map((team, index)=>(
+            <>
+              <Grid item xs={6} sm={3} key={index}>
+                <Paper className={classes.paper}>
+                  <b>Team {index+1}</b>
+                </Paper>
+              </Grid>
 
-          <div className="listGame">
-            <Grid item xs={12} sm={6}>
-              <Paper className={classes.paper}>
-                <p>
-                  <b>Name: </b>
-                  (data.firebase)
-                </p>
-                <small>
-                  <b>T-shirt Number: </b>
-                  (data.firebase)
-                </small>
-              </Paper>
-            </Grid>
-          </div>
-          <Grid item xs={6} sm={3}>
-            <Paper className={classes.paper}>
-              <b>Team 2</b>
-            </Paper>
-          </Grid>
+              {(team||[]).map(playerKey=>(
 
-          <div className="listGame">
-            <Grid item xs={12} sm={6}>
-              <Paper className={classes.paper}>
-                <p>
-                  <b>Name: </b>
-                  (data.firebase)
-                </p>
-                <small>
-                  <b>T-shirt Number: </b>
-                  (data.firebase)
-                </small>
-              </Paper>
-            </Grid>
-          </div>
+                <div className="listGame" key={playerKey}>
+                  <Grid item xs={12} sm={6}>
+                    <Paper className={classes.paper}>
+                      <p>
+                        <b>Name: </b>
+                        {users[playerKey].firstName+" "+users[playerKey].lastName}
+                      </p>
+                      <small>
+                        <b>T-shirt Number: </b>
+                        {users[playerKey].tShirt}
+                      </small>
+                    </Paper>
+                  </Grid>
+                </div>
+              ))}
+            </>
+          ))}
+
         </div>
       </div>
     </>
